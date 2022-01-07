39e4f40de38c31fc73935ba4339027ced23045e4
Austin Taylor <Austin@AMB0003.local>
2022-01-07 15:28:51 -0600

    Added button logic

--- public/app.js
+++ public/app.js
@@ -1,13 +1,16 @@
 const buttonNumber = document.getElementById('button-number');
 const buttonName = document.getElementById('button-name');
+const rootTag = document.getElementById('root');
 
 buttonNumber.addEventListener('click', fNumber);
 buttonName.addEventListener('click', fName);
 
 function fNumber() {
-    console.log('Clicked!')
+    const n1 = Math.floor(Math.random() * 100); //random number between 0-99
+    const n2 = Math.floor(Math.random() * 100); //random number between 0-99
+    rootTag.innerHTML = `Your lucky numbers are ${n1} and ${n2}!`;
 }
 
 function fName() {
-    console.log('Also clicked!')
+    rootTag.innerHTML= 'My name is Austin!';
 }
